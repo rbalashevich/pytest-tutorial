f0a1f61b64c3ce1d4233012e5eabe1e24a0e8c16
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2021-10-27 18:17:20 +0300

    Update wallet_test.py

--- src/wallet_test.py
+++ src/wallet_test.py
@@ -1,29 +1,36 @@
 import pytest
 from wallet import Wallet, InsufficientAmount
 
+
 @pytest.fixture
 def empty_wallet():
     '''Returns a Wallet instance with a zero balance'''
     return Wallet()
+
 
 @pytest.fixture
 def wallet():
     '''Returns a Wallet instance with a balance of 20'''
     return Wallet(20)
 
+
 def test_default_initial_amount(empty_wallet):
     assert empty_wallet.balance == 0
+
 
 def test_setting_initial_amount(wallet):
     assert wallet.balance == 20
 
+
 def test_wallet_add_cash(wallet):
     wallet.add_cash(80)
     assert wallet.balance == 100
+E302 expected 2 blank lines, found 1
 
 def test_wallet_spend_cash(wallet):
     wallet.spend_cash(10)
     assert wallet.balance == 10
+
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
     with pytest.raises(InsufficientAmount):
