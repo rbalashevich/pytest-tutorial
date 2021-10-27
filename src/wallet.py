1ab9c5a69c2bc385462b7f3930d41c986e552a1b
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2021-10-27 18:30:35 +0300

    Update wallet.py

--- src/wallet.py
+++ src/wallet.py
@@ -1,21 +1,25 @@
 import logging
 
+
 class InsufficientAmount(Exception):
     pass
 
 
 class Wallet(object):
 
-    def __init__(self, initial_amount=0):
+
+def __init__(self, initial_amount=0):
         logging.info('Initializing')
         self.balance = initial_amount
 
-    def spend_cash(self, amount):
+
+def spend_cash(self, amount):
         logging.info('Spending')
         if self.balance < amount:
             raise InsufficientAmount('Not enough available to spend {}'.format(amount))
         self.balance -= amount
 
-    def add_cash(self, amount):
+
+def add_cash(self, amount):
         logging.info('Mo money!')
         self.balance += amount
